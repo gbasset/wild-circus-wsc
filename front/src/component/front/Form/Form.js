06fc41a9142ccf058a8847d7de0d93bd2ce989ce
Gaetan <gaetan@protonmail.com>
2020-01-30 18:14:32 +0100

    add the subject map

---
+++ front/src/component/front/Form/Form.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import './Form.css'
+
+export default function Form(props) {
+const [subject2, setSubject2] = useState(props.data)
+
+      console.log('props.data', props.data);
+      
+    return (
+        <>
+        <form>
+            <input type="text" value="zizi"/>
+
+            <label htmlFor="sujet">sujet</label>
+            <select
+              className="custom-select text-center"
+              name="category_name"
+              id="inputGroupSelect01"
+           
+            >
+              <option selected> Selectionner une Catégorie</option>
+              {props.data &&
+                props.data.map(data => {
+                  return <option>{data.object_name} </option>;
+                })}
+            </select>
+  
+        </form>
+
+
+
+        </>
+    )
+}
+
--- front/src/component/front/Pages/Accueil/Accueil.js
+++ front/src/component/front/Pages/Accueil/Accueil.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Header from '../../Header/Header'
+import Form from '../../Form/Form'
 import './Accueil.css'
 import ButtonRepresentation from '../../Button/ButtonRepresentation'
 export default function Accueil() {
 const [pictures, setPictures] = useState([])
+const [subject, setSubject] = useState([])
     const fetchData = () => {
         axios
             .get("/image/all") //liste les commandes
@@ -12,10 +14,19 @@ const [pictures, setPictures] = useState([])
                 setPictures(res.data)
             })
     }
+    const fetchDataSujet= () => {
+        axios
+            .get("/sujet/all") //liste les commandes
+            .then(res => {
+                setSubject(res.data)
+            })
+    }
     useEffect(() => {
         fetchData()
+        fetchDataSujet()
       }, [])
       console.log('pictures', pictures);
+      console.log('sujet', subject);
       
     return (
         <>
@@ -29,6 +40,7 @@ const [pictures, setPictures] = useState([])
 
                 </div>
             </div>
+            <Form data={subject}/>
 
             </div>
 
