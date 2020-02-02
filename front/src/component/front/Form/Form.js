5b9bbc0e3dd07b44f536fa7754d6a987ff333ff5
Gaetan <gaetan@protonmail.com>
2020-02-02 18:46:04 +0100

    add the function for the date message auto

--- front/src/component/front/Form/Form.js
+++ front/src/component/front/Form/Form.js
@@ -1,12 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './Form.css'
+import { Link } from 'react-router-dom';
 
 export default function Form(props) {
+  let getDate = (separator='-') => {
+
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+    return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
+    
+  }
+  let date = getDate();
+
+  console.log('date', date);
+
   const [subject2, setSubject2] = useState(props.data)
+  const [msg, setMsg] = useState({
+    message_client_name : "",
+    message_client_lastname: "",
+    message_client_phone: "",
+    message_client_mail: "",
+    message_message: "",
+    message_objet_id: 3,
+    message_date: date,
 
-  console.log('props.data', props.data);
 
+  })
+
+   
+    const handleSubmit = (e) => {
+      axios.post("/messages/",msg)
+      .then(res => {
+        if (res.err) {
+          alert(res.err);
+        } else {
+          alert(`${msg.message_client_name} votre message a été envoyé avec succès`)
+        }
+      })
+
+      e.preventDefault();
+
+  }
+
+  const updateMsg = (e) => {
+      setMsg({ ...msg, [e.target.name]: e.target.value })
+  }
+  useEffect(() => {
+
+  }, [])
   return (
     <>
       <form className="formulaireresa">
@@ -26,17 +70,18 @@ export default function Form(props) {
 
         </select>
 
-        <input type="text" placeholder="Votre nom" className="inputForm" />
-        <input type="text" placeholder="Votre Prénom" className="inputForm" />
-        <input type="text" placeholder="Votre téléphone" className="inputForm" />
-        <input type="text" placeholder="Votre mail" className="inputForm" />
-        <textarea rows="5" cols="60" placeholder="Votre message" className="textArea" />
+        <input type="text" placeholder="Votre nom" className="inputForm" name="message_client_name" onChange={updateMsg} />
+        <input type="text" placeholder="Votre Prénom" className="inputForm" name="message_client_lastname"onChange={updateMsg}/>
+        <input type="phone" placeholder="Votre téléphone" className="inputForm" name="message_client_phone" onChange={updateMsg}/>
+        <input type="email" multiple placeholder="Votre mail" className="inputForm" name="message_client_mail"onChange={updateMsg} />
+        <textarea rows="5" cols="60" placeholder="Votre message" className="textArea" name="message_message"onChange={updateMsg} />
 
         <div>
 
           <button
             type="submit"
             className="btnSendMsg"
+            onClick={handleSubmit}
           >
             Envoyer
          </button>
