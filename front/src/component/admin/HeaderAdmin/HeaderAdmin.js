2b24842989185a5d79ae8a405c27e8b3dcff050c
Gaetan <gaetan@protonmail.com>
2020-02-03 22:12:13 +0100

    add pictures on crew

--- front/src/component/admin/HeaderAdmin/HeaderAdmin.js
+++ front/src/component/admin/HeaderAdmin/HeaderAdmin.js
@@ -6,7 +6,7 @@ export default function Header() {
         <>
             <div className="Head">
                 <nav className="menu">
-                    <Link to="/" className='logo' > <img src="https://i.postimg.cc/htKwbDHX/logocirque.png"/> </Link>
+                    <Link to="/"  > <img className='logo'src="https://i.postimg.cc/htKwbDHX/logocirque.png"/> </Link>
                     <NavLink className='link' to="/admin" activeStyle={{color:"#7bed9f"}}>Accueil admin</NavLink>
                     <NavLink className='link' to="/representationadmin" activeStyle={{color:"#7bed9f"}} >Representations</NavLink>
                     <NavLink className='link' to="/equipeadmin" activeStyle={{color:"#7bed9f"}}>L'equipe</NavLink>
